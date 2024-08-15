da41120f3fdec5a427febd02a8c4f4a47a4014fd
Sakthi20055 <157904490+Sakthi20055@users.noreply.github.com>
2024-08-15 22:33:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// script.js
+
+const themeToggle = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme') || 'light';
+
+if (currentTheme === 'dark') {
+    document.body.classList.add('dark');
+}
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    const theme = document.body.classList.contains('dark') ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+});
